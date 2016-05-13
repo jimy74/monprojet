d56eb0460f134a12e7ff55ec3d189f9aeb7d0133
Jimmy Paris <jimmyparis74@gmail.com>
2016-05-13 18:55:13 +0200

    Simplification du test de la classe monde
    J'avais fait bien trop compliqué

--- src/test/java/ch/hesge/paris/marmotte/MondeTest.java
+++ src/test/java/ch/hesge/paris/marmotte/MondeTest.java
@@ -5,21 +5,15 @@ import org.testng.annotations.BeforeMethod;
 
 public class MondeTest {
     
-private JeuMarmotteHunter jeu;
+private Monde monde;
 
 @BeforeMethod
 public void setUp() {
-        Parametres p = new Parametres();
-        jeu = new JeuMarmotteHunter(p,               
-                new TimerPerso(new Monde(p.getMondeTailleX(), p.getMondeTailleY()),
-                        p.getVitesseDifficulteEmperique(),
-                        p.getVitesseDifficulte()
-                )
-        );
+    Parametres p = new Parametres();
+    monde = new Monde(p.getMondeTailleX(), p.getMondeTailleY());
 }
     @Test
-    public void getCaseVideAliatoire_retourne_une_case_vraiment_vide() {
-        Monde monde = jeu.getMonde();
+    public void getCaseVideAliatoire_retourne_une_case_vraiment_vide() {      
         assertNotNull(monde.getCaseVideAliatoire());
     }
     
