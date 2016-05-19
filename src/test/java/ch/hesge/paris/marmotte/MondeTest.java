fa841d61e6c859eff29f73aec88e0b03c0016f2e
Jimmy Paris <jimmyparis74@gmail.com>
2016-05-19 22:30:28 +0200

    Ajout d'une méthode de test du Monde Ignorée, car étrangement non fonctionnel
    J'essay de fixé l'aléatoire comme vue en cours sur les Mock avec
    l'utilisation du when().thenReturn() mais c'est comme si je ne l'avais
    pas fait.
    
    L'aléatoire reste indomptable pour le moment ... à résoudre !

--- src/test/java/ch/hesge/paris/marmotte/MondeTest.java
+++ src/test/java/ch/hesge/paris/marmotte/MondeTest.java
@@ -1,6 +1,11 @@
 package ch.hesge.paris.marmotte;
+import java.util.Random;
+import org.junit.Ignore;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import org.testng.annotations.Test;
 import static org.testng.Assert.assertNotNull;
+import static org.testng.Assert.assertTrue;
 import org.testng.annotations.BeforeMethod;
 
 public class MondeTest {
@@ -16,5 +21,26 @@ public void setUp() {
     public void getCaseVideAliatoire_retourne_une_case_vraiment_vide() {      
         assertNotNull(monde.getCaseVideAliatoire());
     }
-    
+
+    @Ignore
+      @Test
+    public void getCaseVideAliatoire_renvoi_la_case_qui_est_au_x_et_y_aleatoire() { 
+        Monde p_monde = mock(Monde.class);
+        Random alea = mock(Random.class);
+        
+        int tailleRandom = 3; //je fige l'entrée de la méthode testée, "si non comment tester l'aléatoire/l'imprévisible"
+        when(alea.nextInt()).thenReturn(3); 
+        
+        Case caseTrouvee = p_monde.getCaseVideAliatoire();
+        
+        System.out.println(caseTrouvee.getPositionX());
+        System.out.println(caseTrouvee.getPositionY());
+        
+        Case caseTrouvee2 = p_monde.getCaseVideAliatoire();
+        
+        System.out.println(caseTrouvee2.getPositionX());
+        System.out.println(caseTrouvee2.getPositionY());        
+        
+        assertTrue(caseTrouvee.getPositionX() == tailleRandom && caseTrouvee.getPositionY() == tailleRandom);
+    }
 }    
