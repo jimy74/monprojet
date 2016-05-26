614053095f8bbfea26432a2508451bf4a7953292
Jimmy Paris <jimmyparis74@gmail.com>
2016-05-26 15:17:25 +0200

    Identation des tests du Monde

--- src/test/java/ch/hesge/paris/marmotte/MondeTest.java
+++ src/test/java/ch/hesge/paris/marmotte/MondeTest.java
@@ -1,4 +1,5 @@
 package ch.hesge.paris.marmotte;
+
 import java.util.Random;
 import org.junit.Ignore;
 import static org.mockito.Matchers.anyInt;
@@ -10,32 +11,33 @@ import static org.testng.Assert.assertTrue;
 import org.testng.annotations.BeforeMethod;
 
 public class MondeTest {
-    
-private Monde monde;
 
-@BeforeMethod
-public void setUp() {
-    Parametres p = new Parametres();
-    monde = new Monde(p.getMondeTailleX(), p.getMondeTailleY());
-}
+    private Monde monde;
+
+    @BeforeMethod
+    public void setUp() {
+        Parametres p = new Parametres();
+        monde = new Monde(p.getMondeTailleX(), p.getMondeTailleY());
+    }
+
     @Test
-    public void getCaseVideAliatoire_retourne_une_case_vraiment_vide() {      
+    public void getCaseVideAliatoire_retourne_une_case_vraiment_vide() {
         assertNotNull(monde.getCaseVideAliatoire(new Random()));
     }
 
-      @Test
-    public void getCaseVideAliatoire_renvoi_la_case_qui_est_au_x_et_y_aleatoire() { 
-        
+    @Test
+    public void getCaseVideAliatoire_renvoi_la_case_qui_est_au_x_et_y_aleatoire() {
+
         Random alea = mock(Random.class);
-        
+
         int tailleRandom = 3; //je fige l'entrée de la méthode testée, "si non comment tester l'aléatoire/l'imprévisible"
-        when(alea.nextInt(anyInt())).thenReturn(tailleRandom); 
-        
-        Case caseTrouvee = monde.getCaseVideAliatoire(alea);             
-        
-        Case caseTrouvee2 = monde.getCaseVideAliatoire(alea);              
-        
-        assertTrue(caseTrouvee.getPositionX() == tailleRandom+1 && caseTrouvee.getPositionY() == tailleRandom+1);
-             
+        when(alea.nextInt(anyInt())).thenReturn(tailleRandom);
+
+        Case caseTrouvee = monde.getCaseVideAliatoire(alea);
+
+        Case caseTrouvee2 = monde.getCaseVideAliatoire(alea);
+
+        assertTrue(caseTrouvee.getPositionX() == tailleRandom + 1 && caseTrouvee.getPositionY() == tailleRandom + 1);
+
     }
-}    
+}
