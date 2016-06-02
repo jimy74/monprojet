ad2a0b0aca5b25bfdc95d6ccbb9a8936f277704a
Jimmy Paris <jimmyparis74@gmail.com>
2016-06-02 18:34:03 +0200

    Correction des codes smells du Monde
    J'ai simplement concentré deux if consécutif en un seul.
    C'est donc une best practice (réduction du volume de code etc...).

--- src/main/java/ch/hesge/paris/marmotte/Monde.java
+++ src/main/java/ch/hesge/paris/marmotte/Monde.java
@@ -11,46 +11,40 @@ import java.util.Random;
  *
  * @author PARIS_JIMMY-ESIG
  */
-public class Monde extends FramePerso{
+public class Monde extends FramePerso {
 
     //Constructeur
     public Monde(int tailleX, int tailleY) {
         super(tailleX, tailleY);  //met le titre de la fenêtre
     }
 
-
-    public boolean toutesCasesPleines(){
-        for (Case uneCase : getCases()){
-            if (uneCase.isVide())
+    public boolean toutesCasesPleines() {
+        for (Case uneCase : getCases()) {
+            if (uneCase.isVide()) {
                 return false;
+            }
         }
         return true;
-        
-                        
-                    
+
     }
-            
+
     /**
-     * Doit impérativement trouver une case aléatoirement
+     * Doit impérativement trouver une case aléatoirement si toutesCasesPleines()
+     *
+     * @param alea
      * @return
      */
-    public Case getCaseVideAliatoire(Random alea) {    
-        if (toutesCasesPleines())
+    public Case getCaseVideAliatoire(Random alea) {
+        if (toutesCasesPleines()) {
             return null;
-        
-        while (true)
-        {
-            
-            for (Case uneCase : getCases())
-            {
-                if (uneCase.isVide())
-                {
-                    //Si la case est bien Ã  la position trouvée aléatoirement
-                    if (alea.nextInt(getTailleX()) + 1 == uneCase.getPositionX() && (alea.nextInt(getTailleY()) + 1 == uneCase.getPositionY())) {
-                        return uneCase; //la case est retenue
-                    }
+        }
+        while (true) {
+            for (Case uneCase : getCases()) {
+                //Si la case est bien vide et à la position trouvée aléatoirement
+                if (uneCase.isVide() && (alea.nextInt(getTailleX()) + 1 == uneCase.getPositionX() && (alea.nextInt(getTailleY()) + 1 == uneCase.getPositionY()))) {
+                    return uneCase; //la case est retenue
                 }
-            } 
+            }
         }
     }
 
