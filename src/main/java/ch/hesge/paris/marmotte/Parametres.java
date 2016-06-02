e501d59d1fde083f5c4e3d891064d8bf93f612a1
Jimmy Paris <jimmyparis74@gmail.com>
2016-06-02 18:37:11 +0200

    Suppression du constructeur implicite

--- src/main/java/ch/hesge/paris/marmotte/Parametres.java
+++ src/main/java/ch/hesge/paris/marmotte/Parametres.java
@@ -22,8 +22,6 @@ public class Parametres {
     private int mondeTailleX = 4;
     private int mondeTailleY = 4;
 
-    public Parametres() {
-    }
     /**
      *
      * @param vitesseDifficulte
